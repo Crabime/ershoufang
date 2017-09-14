4bf5604563e32d9aebd2fee7871fc13f94c9b512
Crabime <crabime@gmail.com>
2017-09-14 11:16:50 +0800

    增加自动化登录流程

--- test/ganji_unittest.py
+++ test/ganji_unittest.py
@@ -13,7 +13,13 @@ from server.gaoni_proxy import *
 
 class GanjiTest(unittest.TestCase):
     def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://wh.ganji.com/fang5/wuchang/")
+
+    def proxy_prerequisite(self):
+        """使用代理的配置"""
         self._proxy = get_random_proxy()
+        print(self._proxy)
         chrome_options = Options()
         chrome_options.add_argument('--proxy-server=http://%s' % self._proxy)
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
@@ -27,7 +33,6 @@ class GanjiTest(unittest.TestCase):
         next_page = WebDriverWait(self.driver, timeout=7).until(lambda x: x.find_element_by_xpath(xpath='//a[@class="next"]'), message="获取下一页元素超时")
         action = ActionChains(self.driver)
         action.move_to_element(next_page).perform()
-        time.sleep(3)
 
     def testLogin(self):
         """
@@ -35,8 +40,24 @@ class GanjiTest(unittest.TestCase):
 
         """
         login = WebDriverWait(self.driver, timeout=3).until(lambda x: x.find_element_by_xpath(xpath='//a[contains(@class, "login")]'), message="无法找到登录按钮")
+        home_login_action = ActionChains(self.driver)
+        home_login_action.click(login).perform()
+        time.sleep(3)
+        print(self.driver.current_url)  # 注意这里又一个重定向过程
+        login_icon = WebDriverWait(self.driver, timeout=3).until(lambda x: x.find_element_by_css_selector('i.tipIcon'), message="无法找到密码登录按钮")
         login_action = ActionChains(self.driver)
-        login_action.click(login).perform()
+        login_action.click(login_icon).perform()
+        time.sleep(2)
+        username = self.driver.find_element_by_name("login_username")
+        username.clear()
+        username.send_keys("15527811219")
+        password = self.driver.find_element_by_name("login_password")
+        password.clear()
+        password.send_keys("a123456")
+        # todo 增加验证码识别
+        submit_button = WebDriverWait(self.driver, timeout=3).until(lambda x: x.find_element_by_css_selector('input.submit'), message="表单无法提交")
+        submit_action = ActionChains(self.driver)
+        submit_action.click(submit_button).perform()
 
     def testGetSinglePageItem(self):
         """
@@ -52,4 +73,5 @@ class GanjiTest(unittest.TestCase):
             print('\n')
 
     def tearDown(self):
+        time.sleep(3)
         self.driver.close()
